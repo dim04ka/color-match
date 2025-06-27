bbac0a4eeaeb313765bbb1102ccd409275a8a4f6
dim04ka <dmitry.suhotsky@gmail.com>
2025-06-27 12:59:20 +0300

    added alias

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
+    resolve: {
+        alias: {
+            '@': '/src',
+            '@app': '/src/app',
+            '@pages': '/src/pages',
+            '@components': '/src/components',
+            '@assets': '/src/assets',
+            '@constants': '/src/constants',
+            '@shared': '/src/shared',
+        },
+    },
 })
